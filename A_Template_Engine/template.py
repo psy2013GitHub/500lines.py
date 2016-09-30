efa82c1d45b55db029aad3dc5c6c93d94e66cf97
deng.zhou <deng.zhou@immomo.com>
2016-09-30 17:40:06 +0800

    1

--- A_Template_Engine/template.py
+++ A_Template_Engine/template.py
@@ -21,18 +21,29 @@ class Template:
         var_code = code.add_section()
         code.add_line("return res")
 
-        stack = []
+        buffer = []
+        def add_code(list_in):
+            if len(list_in) == 0:
+                buffer.append("extend_res([%s,])" % ','.join(list_in))
+            else:
+                buffer.append("append_res(%s)" % repr(list_in))
+
+        control_stack = []
         for line in template_str.split('\n'):
             line = line.strip()
-            tokens = token_matcher.split(line)
+            tokens = [_.strip() for _ in token_matcher.split(line)]
             if tokens[0].startswith('{{'):
-                code.add_line("")
+                add_code(tokens[0][2:-2])
             elif tokens[0].startswith('{% end'):
-                tmp = []
-                tmp.append(stack.pop())
+                end_sgn = tokens[0][3:] # fixme
+                if not control_stack:
+                    raise ValueError
+                start_sgn = control_stack.pop()
+                if end_sgn != start_sgn:
+                    raise ValueError
             elif tokens[0].startswith('{%'):
                 tmp = "for %s in %s:" % (tokens[1].strip(), tokens[2].strip())
-                stack.append(tmp)
+                add_code(tmp)
                 code.indent()
             elif tokens[0].startswith('{#'): # 注释
                 continue
